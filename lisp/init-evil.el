604545d6b9f9b5a652c04b8cab425dff8715e86a
tywhisky <fty221@gmail.com>
2023-11-17 19:22:28 +0800

    Update

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,7 +2,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+;;(package-initialize)
 (package-refresh-contents)
 
 ;; Download Evil
