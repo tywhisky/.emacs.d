20a8cbb29a005caa46075e4b7311c1652dcee21c
tywhisky <fty221@gmail.com>
2024-08-29 21:12:09 +0800

    Enable Ido

--- init.el
+++ init.el
@@ -9,6 +9,11 @@
 (setq make-backup-files nil)
 (setq inhibit-startup-screen t)
 
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
 (defun my-init-file ()
   "Open the init file."
   (interactive)
