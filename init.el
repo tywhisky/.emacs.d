7e467ea4f0f3a5d79b6921ccd71254416a71ad56
tywhisky <fty221@gmail.com>
2024-08-29 20:24:17 +0800

    Update

---
+++ init.el
@@ -0,0 +1,10 @@
+(set-frame-font "Iosevka 22" nil t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(add-to-list 'default-frame-alist '(height . 34))
+(add-to-list 'default-frame-alist '(width . 80))
+
+(setq make-backup-files nil)
+(setq inhibit-startup-screen t)
