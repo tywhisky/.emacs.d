5218c996a3861acfcae9f6ec7d1db9663b03d635
tywhisky <fty221@gmail.com>
2024-08-29 22:59:16 +0800

    Update

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet markdown-mode corfu magit with-editor transient dash)))
+   '(elixir-mode yasnippet markdown-mode magit with-editor transient dash)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
