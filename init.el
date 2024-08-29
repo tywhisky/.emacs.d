787339d28785ec763c88b8c9e3e0ccfe7594a2ca
tywhisky <fty221@gmail.com>
2024-08-29 21:26:05 +0800

    Add corfu

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(add-to-list 'default-frame-alist '(height . 34))
+(add-to-list 'default-frame-alist '(height . 38))
 (add-to-list 'default-frame-alist '(width . 80))
 
 (setq make-backup-files nil)
@@ -19,15 +19,16 @@
   (interactive)
   (find-file user-init-file))
 
+(setq corfu-auto t)
+
 (add-to-list 'package-archives
-             (cons "melpa" "https://melpa.org/packages/")
-             t)
+	     (cons "melpa" "https://melpa.org/packages/") t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit with-editor transient dash)))
+ '(package-selected-packages '(corfu magit with-editor transient dash)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
