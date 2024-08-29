02b251495be6615dfa877e7ddf6872b48b1f319c
tywhisky <fty221@gmail.com>
2024-08-29 21:05:04 +0800

    Update

--- init.el
+++ init.el
@@ -8,3 +8,24 @@
 
 (setq make-backup-files nil)
 (setq inhibit-startup-screen t)
+
+(defun my-init-file ()
+  "Open the init file."
+  (interactive)
+  (find-file user-init-file))
+
+(add-to-list 'package-archives
+             (cons "melpa" "https://melpa.org/packages/")
+             t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit with-editor transient dash)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
