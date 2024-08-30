ddd6cce5c1a7d6c437b2d3e9d713ef85a5717423
tywhisky <fty221@gmail.com>
2024-08-30 14:36:13 +0800

    Change to eglot

--- init.el
+++ init.el
@@ -19,14 +19,10 @@
   (interactive)
   (find-file user-init-file))
 
-(setq corfu-auto t)
-
-(add-to-list 'load-path "~/.config/emacs/lisp-site/lsp-bridge/")
-(setq lsp-bridge-elixir-lsp-server "elixirLS")
-(require 'yasnippet)
-(yas-global-mode 1)
-(require 'lsp-bridge)
-(global-lsp-bridge-mode)
+(add-hook 'after-init-hook 'global-company-mode)
+(require 'eglot)
+(add-hook 'elixir-mode-hook 'eglot-ensure)
+(add-to-list 'eglot-server-programs '(elixir-mode "~/.config/emacs/language-server/elixir/release/language_server.sh"))
 
 (add-to-list 'package-archives
 	     (cons "melpa" "https://melpa.org/packages/") t)
@@ -36,7 +32,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elixir-mode yasnippet markdown-mode magit with-editor transient dash)))
+   '(company elixir-mode yasnippet markdown-mode magit with-editor transient dash)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
