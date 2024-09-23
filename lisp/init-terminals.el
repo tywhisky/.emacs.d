04c253920c16911b60c5fba03a3f44fa61aff01a
tywhisky <fty221@gmail.com>
2024-09-23 17:19:30 +0800

    Fix font error for eat

--- lisp/init-terminals.el
+++ lisp/init-terminals.el
@@ -3,7 +3,6 @@
 ;;; Commentary:
 
 ;;; Code:
-
 (when (maybe-require-package 'eat)
   (defun sanityinc/on-eat-exit (process)
     (when (zerop (process-exit-status process))
@@ -12,21 +11,14 @@
         (delete-window))))
   (add-hook 'eat-exit-hook 'sanityinc/on-eat-exit)
 
+  (setq eat-term-name "xterm-256color")
+
   (with-eval-after-load 'eat
     (custom-set-variables
      `(eat-semi-char-non-bound-keys
        (quote ,(cons [?\e ?w] eat-semi-char-non-bound-keys)))))
 
-  (defun sanityinc/eat-window-on-current-path ()
-    "Run `eat-other-window` and cd to current path."
-    (interactive)
-    (let ((current-dir (file-name-directory (buffer-file-name))))
-      (eat-other-window)
-      (run-at-time 0.1 nil
-		   (lambda ()
-                     (eat-term-send-string eat-terminal (concat "cd " current-dir))
-                     (eat-term-send-string eat-terminal "\r")))))
-
-  (global-set-key (kbd "C-c t") 'sanityinc/eat-window-on-current-path))
+  (global-set-key (kbd "C-c t") 'eat-other-window))
 
 (provide 'init-terminals)
+;;; init-terminals.el ends here
