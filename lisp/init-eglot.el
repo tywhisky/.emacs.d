4ad986bb74c6612300ea815c8b78fdd043a342d3
tywhisky <fty221@gmail.com>
2024-09-23 15:38:31 +0800

    Add config for language server

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -5,5 +5,9 @@
 (when (maybe-require-package 'eglot)
   (maybe-require-package 'consult-eglot))
 
+(with-eval-after-load 'eglot
+ (add-to-list 'eglot-server-programs '(elixir-mode "~/.config/emacs/language-server/elixir/release/language_server.sh")))
+
+
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
