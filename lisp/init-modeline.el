d0bd0a92b9ca569e1999a9de5d1ade7a11bb45cd
tywhisky <fty221@gmail.com>
2024-09-25 17:40:55 +0800

    Update modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -2,8 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'doom-modeline)
-
+(maybe-require-package 'doom-modeline)
 (doom-modeline-mode 1)
+
+(setq doom-modeline-height 23)
+
+;; doom-modeline-position-column-line-format need
+;; both column-number-mode and line-number-mode enabled
+(column-number-mode 1)
+(line-number-mode 1)
+(setq doom-modeline-position-column-line-format '("%l:%c"))
+
+(display-time-mode 1)
+(display-battery-mode 1)
 
 (provide 'init-modeline)
