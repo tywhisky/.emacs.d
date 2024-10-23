6cdfab058b8760965ac6e00a59ae93d0057b3dc7
tywhisky <fty221@gmail.com>
2024-10-23 11:11:18 +0800

    Delete language servers

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -9,7 +9,7 @@
   (add-to-list 'eglot-server-programs '(elixir-mode "~/.config/emacs/language-server/elixir/release/language_server.sh"))
   (add-to-list 'eglot-server-programs
                '((js-ts-mode typescript-ts-mode tsx-ts-mode)
-                 "typescript-language-server" "--stdio")))
+                 "/Users/taine/.local/share/mise/installs/node/23.0.0/bin/typescript-language-server" "--stdio")))
 
 (add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
