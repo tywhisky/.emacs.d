acb98919fe607b50ec9bdcca1f4c404dbb5273d3
tywhisky <fty221@gmail.com>
2024-10-23 16:39:24 +0800

    The ls for typescript still use the package version

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -8,8 +8,8 @@
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '(elixir-ts-mode "~/.language-servers/elixir-ls/release/language_server.sh"))
   (add-to-list 'eglot-server-programs
-               '((js-ts-mode typescript-ts-mode tsx-ts-mode)
-                 "/Users/taine/.local/share/mise/installs/node/23.0.0/bin/typescript-language-server" "--stdio")))
+               '((js-ts-mode typescript-ts-mode)
+                 "typescript-language-server" "--stdio")))
 
 (add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
