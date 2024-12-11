c8b70860de62d659920845e2058617d884af513a
Taine Fan <fty221@gmail.com>
2024-12-11 17:40:56 +0800

    Switch to TNU mirror

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -12,7 +12,14 @@
 
 ;;; Standard package repositories
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+
+;; TNUA ELPA
+(setq package-archives
+      '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
 
