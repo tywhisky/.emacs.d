63c409ecc5195a34d871d704e036a4e2525c7ff6
Taine Fan <fty221@gmail.com>
2024-12-20 11:23:32 +0800

    Rebind suspend-frame

--- lisp/init-key-mapping.el
+++ lisp/init-key-mapping.el
@@ -6,5 +6,9 @@
       mac-option-modifier       'alt
       mac-right-option-modifier 'alt)
 
+;; Rebind the 'suspend-frame especially on WSL
+(global-set-key (kbd "C-x 5") 'suspend-frame)
+(global-unset-key (kbd "C-z"))
+
 (provide 'init-key-mapping)
 ;;; init-key-mapping.el ends here
