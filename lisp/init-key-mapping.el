00e71a12eece020b88b217fb0c23060dd6b98330
Taine Fan <fty221@gmail.com>
2024-12-25 17:09:47 +0800

    Rebind <C-[> to keyboard-quit

--- lisp/init-key-mapping.el
+++ lisp/init-key-mapping.el
@@ -2,13 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;; Change META key on MacOS
 (setq mac-command-modifier      'meta
       mac-option-modifier       'alt
       mac-right-option-modifier 'alt)
 
+
 ;; Rebind the 'suspend-frame especially on WSL
 (global-set-key (kbd "C-x 5") 'suspend-frame)
 (global-unset-key (kbd "C-z"))
+
+
+;; Rebind <C-[> (ESC) to C-g (keyboard-quit)
+(defun setup-input-decode-map ()
+  (define-key input-decode-map (kbd "C-[") (kbd "C-g")))
+
+(setup-input-decode-map)
+(add-hook 'tty-setup-hook #'setup-input-decode-map)
 
 (provide 'init-key-mapping)
 ;;; init-key-mapping.el ends here
