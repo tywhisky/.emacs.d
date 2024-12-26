3710f9be8e2638d1f1c0359d3c134e44eb8da9a5
Taine Fan <fty221@gmail.com>
2024-12-26 15:36:29 +0800

    Add dimmer and set config for corfu

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -9,7 +9,9 @@
 ;; (maybe-require-package 'color-theme-sanityinc-tomorrow)
 (maybe-require-package 'ef-themes)
 
+
 ;; Set the default theme for terminal and GUI emacs
+
 (if (display-graphic-p)
     (load-theme 'ef-light t) 
   (load-theme 'ef-night t))
@@ -18,6 +20,46 @@
   (custom-set-faces
    `(fill-column-indicator ((,c :height 0.10)))))
 
+
+
+(defun advise-dimmer-config-change-handler ()
+  "Advise to only force process if no predicate is truthy."
+  (let ((ignore (cl-some (lambda (f) (and (fboundp f) (funcall f)))
+                         dimmer-prevent-dimming-predicates)))
+    (unless ignore
+      (when (fboundp 'dimmer-process-all)
+        (dimmer-process-all t)))))
+
+(defun corfu-frame-p ()
+  "Check if the buffer is a corfu frame buffer."
+  (string-match-p "\\` \\*corfu" (buffer-name)))
+
+(defun dimmer-configure-corfu ()
+  "Convenience settings for corfu users."
+  (add-to-list
+   'dimmer-prevent-dimming-predicates
+   #'corfu-frame-p))
+
+(when (maybe-require-package 'dimmer)
+  (setq-default dimmer-fraction 0.3)
+  (setq-default dimmer-adjustment-mode :foreground)
+  (setq-default dimmer-use-colorspace :rgb)
+  (add-hook 'after-init-hook 'dimmer-mode)
+  (with-eval-after-load 'dimmer
+    ;; TODO: file upstream as a PR
+    (advice-add 'frame-set-background-mode :after (lambda (&rest args) (dimmer-process-all))))
+  (with-eval-after-load 'dimmer
+    ;; Don't dim in terminal windows. Even with 256 colours it can
+    ;; lead to poor contrast.  Better would be to vary dimmer-fraction
+    ;; according to frame type.
+    (advice-add
+     'dimmer-config-change-handler
+     :override 'advise-dimmer-config-change-handler)
+    (dimmer-configure-corfu)
+    (defun sanityinc/display-non-graphic-p ()
+      (not (display-graphic-p)))
+    (add-to-list 'dimmer-exclusion-predicates 'sanityinc/display-non-graphic-p)))
+
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
