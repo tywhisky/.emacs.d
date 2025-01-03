d583eadd2994970e4e2776df01125598f41e9035
Taine Fan <fty221@gmail.com>
2025-01-03 15:38:02 +0800

    Switch to offical mirror

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -10,18 +10,18 @@
       (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
                         user-emacs-directory))
 
-;;; Standard package repositories
-
-;; (add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
-
+
 ;; TNUA ELPA
-(setq package-archives
-      '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+;; (add-to-list 'package-archives '("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/") t)
+;; (add-to-list 'package-archives '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
+;; (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 
-;; Official MELPA Mirror, in case necessary.
-;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
+;;; Standard package repositories
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(add-to-list 'package-unsigned-archives "melpa")
+
+
 
 ;;; On-demand installation of packages
 
