db4e923596bc7de55d4bcdaa8faf001b1628d136
Taine Fan <fty221@gmail.com>
2025-01-03 15:37:34 +0800

    Modify the path of language server

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -7,17 +7,14 @@
 
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '(elixir-ts-mode "~/.language-servers/elixir-ls/release/language_server.sh"))
-  (add-to-list 'eglot-server-programs
-               '((js-ts-mode typescript-ts-mode)
-                 "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs '((js-ts-mode typescript-ts-mode) "typescript-language-server" "--stdio"))
   (add-to-list 'eglot-server-programs '(go-ts-mode "gopls"))
-  (add-to-list 'eglot-server-programs
-               '((csharp-mode csharp-ts-mode)
-                 "~/.language-servers/dotnet/OmniSharp" "-lsp")))
+  (add-to-list 'eglot-server-programs '(csharp-mode "~/.language-servers/dotnet/OmniSharp" "-lsp")))
 
 (add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
 (add-hook 'go-ts-mode-hook 'eglot-ensure)
+(add-hook 'csharp-mode 'eglot-ensure)
 
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
