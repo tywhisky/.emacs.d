1e7d99e0b84e13bb0e515d170e0c64392be4865f
Taine Fan <fty221@gmail.com>
2025-06-04 14:21:54 +0800

    Use gemini as default model

--- lisp/init-gpt.el
+++ lisp/init-gpt.el
@@ -4,11 +4,21 @@
 
 (maybe-require-package 'gptel)
 
+;; Gemini as default model
+(setq gptel-model 'gemini-2.5-flash-preview-05-20
+          gptel-backend (gptel-make-gemini "Gemini"
+                                           :key (auth-source-pick-first-password :host "gemini")
+                                           :stream nil)
+          gptel-use-curl nil)
+
 (setq gptel-api-key (auth-source-pick-first-password :host "openai"))
-(gptel-make-gemini "Gemini" :key (auth-source-pick-first-password :host "gemini") :stream t)
+
 (gptel-make-deepseek "DeepSeek"       
   :stream t                           
-  :key (auth-source-pick-first-password :host "deepseek"))  
+  :key (auth-source-pick-first-password :host "deepseek"))
+
+(setq gptel-log-level 'info)
 
 (provide 'init-gpt)
 ;;; init-gpt.el ends here
+
