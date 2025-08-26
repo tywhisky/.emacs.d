f85032a8832b455374d17abef10ea983b09d41f8
Taine Fan <fty221@gmail.com>
2025-08-26 14:46:10 +0800

    Auto enable copilot in specify major modes

--- lisp/init-copilot.el
+++ lisp/init-copilot.el
@@ -4,8 +4,6 @@
 
 (require 'copilot)
 
-(copilot-mode 1)
-
 (with-eval-after-load 'copilot
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
@@ -18,6 +16,11 @@
 (add-to-list 'copilot-indentation-alist '(typescript-ts-mode 2))
 (add-to-list 'copilot-indentation-alist '(text-mode 2))
 (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+
+(add-hook 'typescript-ts-mode-hook #'copilot-mode)
+(add-hook 'elixir-ts-mode-hook #'copilot-mode)
+(add-hook 'text-mode-hook #'copilot-mode)
+(add-hook 'emacs-lisp-mode-hook #'copilot-mode)
 
 (provide 'init-copilot)
 ;;; init-copilot.el ends here
