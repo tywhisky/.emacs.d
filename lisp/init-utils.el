ea5899b17ef40ee9bfa67d8e2e820449f6274414
Taine Fan <fty221@gmail.com>
2025-08-27 14:29:45 +0800

    Add function for read scripts on current project(Elixir or Typescript)

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -34,5 +34,64 @@
 
 (global-set-key (kbd "C-c i") 'tywhisky/open-init-file)
 
+
+(defun my/colorize-compilation-buffer ()
+  "Interpret ANSI color codes in compilation buffers."
+  (ansi-color-apply-on-region compilation-filter-start (point)))
+
+(add-hook 'compilation-filter-hook 'my/colorize-compilation-buffer)
+
+(defun tywhisky/project-root ()
+  "Find the project root by looking for mix.exs or package.json."
+  (or (locate-dominating-file default-directory "mix.exs")
+      (locate-dominating-file default-directory "package.json")
+      (user-error "Not in a mix or npm project")))
+
+(defun tywhisky/read-package-json-scripts (root)
+  "Read scripts from package.json in ROOT."
+  (let* ((pkg-file (expand-file-name "package.json" root))
+         (json-object-type 'hash-table)
+         (json-key-type 'string)
+         (json-array-type 'list)
+         (data (json-read-file pkg-file)))
+    (gethash "scripts" data)))
+
+(defun tywhisky/read-mix-tasks (root)
+  "Read mix tasks from mix.exs in ROOT (very rough parse)."
+  (let* ((file (expand-file-name "mix.exs" root))
+         (content (with-temp-buffer
+                    (insert-file-contents file)
+                    (buffer-string)))
+         (aliases '()))
+    (when (string-match "defp aliases do[[:space:]]*%{" content)
+      (save-match-data
+        (with-temp-buffer
+          (insert content)
+          (goto-char (point-min))
+          (while (re-search-forward "\"\\([^\"]+\\)\"" nil t)
+            (push (match-string 1) aliases)))))
+    (nreverse aliases)))
+
+(defun tywhisky/run-project-script ()
+  "List project scripts from mix.exs or package.json and run one."
+  (interactive)
+  (let* ((root (tywhisky/project-root))
+         (scripts (cond
+                   ((file-exists-p (expand-file-name "package.json" root))
+                    (tywhisky/read-package-json-scripts root))
+                   ((file-exists-p (expand-file-name "mix.exs" root))
+                    (tywhisky/read-mix-tasks root))))
+         (choices (if (hash-table-p scripts)
+                      (hash-table-keys scripts)
+                    scripts))
+         (cmd (completing-read "Run script: " choices)))
+    (cond
+     ;; npm/yarn/pnpm
+     ((hash-table-p scripts)
+      (compile (format "npm run %s" cmd)))
+     ;; mix alias
+     (t
+      (compile (format "mix %s" cmd))))))
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
