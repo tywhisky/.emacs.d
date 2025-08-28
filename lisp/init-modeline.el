a1f8fe7bf25eadb7eba628df0967564adb01c0ee
Taine Fan <fty221@gmail.com>
2025-08-28 14:54:09 +0800

    Remove doom-modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,43 +1,42 @@
-;;; init-modeline.el --- Configuration for modeline -*- lexical-binding: t -*-
+;;; init-modeline.el --- Custom native modeline -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'doom-modeline)
-(doom-modeline-mode 1)
+;; Enable line and column numbers in the modeline
+(line-number-mode 1)
+(column-number-mode 1)
 
-
-;; Configuration for modeline
-
-;; Remove modeline border
+;; Remove the box around the modeline
 (set-face-attribute 'mode-line nil :box nil)
 
-
-;; Configuration for doom-modeline
-
-;; doom-modeline-position-column-line-format need
-;; both column-number-mode and line-number-mode enabled
-(column-number-mode 1)
-(line-number-mode 1)
-(setq doom-modeline-position-column-line-format '("%l:%c"))
-
-(setq doom-modeline-height 25)
-(setq doom-modeline-icon nil)
-(setq doom-modeline-buffer-encoding nil)
-(setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
-
-(set-face-background 'doom-modeline-bar (face-background 'mode-line))
-(set-face-background 'doom-modeline-bar-inactive (face-background 'mode-line-inactive))
-(setq doom-modeline-bar-width 1)
-
-(let ((family "Roboto Mono")
+;; Font settings for modeline
+(let ((family "Lilex Nerd Font Mono")
       (slant 'italic)
       (height 0.95)
       (weight 'light)
       (box nil))
   (custom-set-faces
    `(mode-line ((t (:family ,family :slant ,slant :height ,height :weight ,weight :box ,box))))
-   `(mode-line-active ((t (:family ,family :slant ,slant :height ,height :weight ,weight :box ,box))))
    `(mode-line-inactive ((t (:family ,family :slant ,slant :height ,height :weight ,weight :box ,box))))))
+
+;; Custom modeline format
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                ;; buffer modified status
+                (:eval (if (buffer-modified-p) "● " "  "))
+                ;; buffer name
+                mode-line-buffer-identification
+                "   "
+                ;; major mode
+                mode-line-modes
+                "   "
+                ;; position: line:column
+                (:eval (format "%l:%c"))
+                "   "
+                ;; file encoding
+                mode-line-mule-info
+                mode-line-end-spaces))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
