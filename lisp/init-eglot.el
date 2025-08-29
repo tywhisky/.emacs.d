9dd761021b95c31fb2229aa01c068a0d506a1a8a
Taine Fan <fty221@gmail.com>
2025-08-29 11:34:43 +0800

    Change the language server for Elixir to expert

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -6,12 +6,10 @@
   (maybe-require-package 'consult-eglot))
 
 (with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '(elixir-ts-mode "~/.language-servers/elixir-ls/release/language_server.sh"))
   (add-to-list 'eglot-server-programs '((js-ts-mode typescript-ts-mode) "typescript-language-server" "--stdio"))
   (add-to-list 'eglot-server-programs '(go-ts-mode "gopls"))
   (add-to-list 'eglot-server-programs '(csharp-mode "~/.language-servers/dotnet/OmniSharp" "-lsp")))
 
-(add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
 (add-hook 'go-ts-mode-hook 'eglot-ensure)
 (add-hook 'csharp-mode 'eglot-ensure)
--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -2,12 +2,27 @@
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'elixir-mode)
 (maybe-require-package 'elixir-ts-mode)
+
+(let ((lang-server (expand-file-name "~/.language-servers/elixir")))
+  (setenv "PATH" (concat lang-server path-separator (getenv "PATH")))
+  (add-to-list 'exec-path lang-server))
+
+(with-eval-after-load 'eglot
+  (setf (alist-get '(elixir-mode elixir-ts-mode heex-ts-mode)
+                   eglot-server-programs
+                   nil nil #'equal)
+        (if (and (fboundp 'w32-shell-dos-semantics)
+                 (w32-shell-dos-semantics))
+            '("expert_darwin_arm64")
+          (eglot-alternatives
+           '("expert_darwin_arm64" "start_lexical.sh")))))
 
 (add-hook 'elixir-ts-mode-hook
           (lambda ()
             (add-hook 'before-save-hook #'eglot-format nil t)))
+
+(add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 
 (provide 'init-elixir)
 ;;; init-elixir.el ends here
