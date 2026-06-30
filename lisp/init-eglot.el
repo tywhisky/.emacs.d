98f6cc434cec13f15880d3144df47263d1ccc6f8
Taine Fan <fty221@gmail.com>
2026-06-30 16:52:40 +0800

    Add DOTNET_ROOT setup for C# eglot

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -8,11 +8,12 @@
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '((js-ts-mode typescript-ts-mode) "typescript-language-server" "--stdio"))
   (add-to-list 'eglot-server-programs '(go-ts-mode "gopls"))
-  (add-to-list 'eglot-server-programs '(csharp-mode "~/.language-servers/dotnet/OmniSharp" "-lsp")))
+  (add-to-list 'eglot-server-programs '((csharp-mode csharp-ts-mode) "~/.language-servers/dotnet/OmniSharp" "-lsp")))
 
 (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
 (add-hook 'go-ts-mode-hook 'eglot-ensure)
 (add-hook 'csharp-mode-hook 'eglot-ensure)
+(add-hook 'csharp-ts-mode-hook 'eglot-ensure)
 (add-hook 'haskell-mode-hook 'eglot-ensure)
 
 (provide 'init-eglot)
--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -10,6 +10,7 @@
 		 "GPG_AGENT_INFO"
 		 "LANG"
 		 "LC_CTYPE"
+                 "DOTNET_ROOT"
 		 "NIX_SSL_CERT_FILE"
 		 "NIX_PATH"))
     (add-to-list 'exec-path-from-shell-variables var)))
@@ -19,6 +20,18 @@
           (unless (memq system-type '(ms-dos windows-nt))
             (daemonp)))
   (exec-path-from-shell-initialize))
+
+(unless (getenv "DOTNET_ROOT")
+  (let* ((dotnet (executable-find "dotnet"))
+         (root (and dotnet
+                    (directory-file-name
+                     (file-name-directory (file-truename dotnet)))))
+         (default-root "/usr/local/share/dotnet"))
+    (cond
+     ((and root (file-directory-p (expand-file-name "sdk" root)))
+      (setenv "DOTNET_ROOT" root))
+     ((file-directory-p (expand-file-name "sdk" default-root))
+      (setenv "DOTNET_ROOT" default-root)))))
 
 (provide 'init-exec-path)
 ;;; init-exec-path.el ends here
